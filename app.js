c75ca69623ea41c65fe3840f28d7e39eef176eb4
Alik Khilazhev <alikhil@mail.ru>
2016-11-22 17:12:25 +0300

    Updates example.
    * Adds support of using sttp on client and server side.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var searching = require("searching");
 var browserify = require("browserify");
+var DataPacker = require("sttp").DataPacker;
 
 app.get("/bundle.js", function(req, res) {
     res.setHeader('content-type', 'application/javascript');
@@ -17,9 +18,17 @@ app.get("/", function (req, res) {
 	res.sendfile("webapps/view/index.html")
 });
 
+var aesKey = "0000111144442222";
+
 app.get("/search", function (req, res) {
 	var query = req.query["search_query"];
+	var sttpUsed = req.query["sttp_used"] == "true";
+
 	var data = searching.word(query, 250, "data.txt", "./", function(data) {
+		if (sttpUsed) {
+			var packer = new DataPacker(aesKey);
+			data = packer.pack(data);
+		}
 		res.send(data);
 	});
 });
--- webapps/js/script.js
+++ webapps/js/script.js
@@ -1,16 +1,19 @@
 var aes = require("../../node_modules/sttp/src/aes.js");
+var DataPacker = require("sttp").DataPacker;
+var aesKey = "0000111144442222";
 
 $(function(){
 	$("#link").click(function (event) {
         console.log("clicked search btn");
-        var text = $("#link_id").val();
+        var text = $("#query").val();
+        var useSTTP = $("#sttp_used").is(":checked");
         console.log(text);
         $.ajax({
             type: "GET",
             url: "/search",
             dataType: "text",
-            data: {search_query: text},
-            success: getSuccess,
+            data: {search_query: text, sttp_used: useSTTP},
+            success: onSuccess,
             error: function (jqXHR, status, errorThrown) {
                 console.log(status);
                 console.log(jqXHR);
@@ -18,5 +21,15 @@ $(function(){
             }
     	});
     });
+
+    function onSuccess(data, status) {
+        var useSTTP = $("#sttp_used").is(":checked");
+        console.log(useSTTP);
+        if (useSTTP) {
+    		var packer = new DataPacker(aesKey);
+    		data = packer.unpack(data);
+    	}
+    	getSuccess(data, status);
+    }
 	console.log(aes.generateKey());
 });
